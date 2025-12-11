f357a11493e6740f77de539466954cc6d36f6f27
Tatiana <djembuka@gmail.com>
2025-12-11 10:26:53 +0300

    feat: adds control

--- bitrix-extensions/local/js/local/vue-apps/design-system/dist/application.bundle.js
+++ bitrix-extensions/local/js/local/vue-apps/design-system/dist/application.bundle.js
@@ -1790,4 +1790,5 @@
 
   exports.DesignSystem = DesignSystem;
 
-}((this.BX = this.BX || {}),BX.Vue3,BX.Vue3.VueRouter,BX.Controls,BX.Modals,BX.Modals,BX.AAS,BX.Controls,BX.AAS,BX.Loaders,BX.Loaders,BX.Loaders,BX.AAS,BX.Vue3.Pinia));//# sourceMappingURL=application.bundle.js.map
+}((this.BX = this.BX || {}),BX,BX,BX.Controls,BX.Modals,BX.Modals,BX.AAS,BX.Controls,BX.AAS,BX.Loaders,BX.Loaders,BX.Loaders,BX.AAS,BX));
+//# sourceMappingURL=application.bundle.js.map
