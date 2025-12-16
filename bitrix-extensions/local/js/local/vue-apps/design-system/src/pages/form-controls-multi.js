1d3df0ce80e26cb34337c3c3d9ea54b5c6fa14f9
Tatiana <djembuka@gmail.com>
2025-12-16 16:00:05 +0300

    feat(design-system): adds submenu

--- bitrix-extensions/local/js/local/vue-apps/design-system/src/pages/form-controls-multi.js
+++ bitrix-extensions/local/js/local/vue-apps/design-system/src/pages/form-controls-multi.js
@@ -1,10 +1,37 @@
 import { FormControlsMultiComponent } from '../components/form-controls-multi';
 
 export const FormControlsMulti = {
+  data() {
+    return {
+      submenu: [
+        {
+          path: 'text',
+          name: 'text'
+        },
+        {
+          path: 'textarea',
+          name: 'textarea'
+        }
+      ]
+    }
+  },
   components: {
     FormControlsMultiComponent,
   },
   template: `
+    <div class="twpx-desing-system-submenu">
+
+      <router-link v-for="item in submenu"
+        :key="item.path"
+        :to="item.path"
+        @click="click(item.path)"
+      >
+        {{ item.name }}
+      </router-link>
+
+    </div>
+
+
     <pre>&lt;ControlChoice
     :control="control"
     @input="input"
@@ -19,4 +46,17 @@ export const FormControlsMulti = {
     <a href="/markup/vue/design-system/controls-store.js">controls-store.js</a>
     <div><FormControlsMultiComponent /></div>
   `,
+  methods: {
+    click(tab) {
+      const url = new URL(window.location.href);
+      if (tab && tab !== '') {
+        url.searchParams.set('tab', `/multi/${tab}`);
+      } else {
+        url.searchParams.delete('tab');
+      }
+      
+      // Обновляем URL
+      window.history.replaceState({}, '', url.toString());
+    }
+  }
 };
