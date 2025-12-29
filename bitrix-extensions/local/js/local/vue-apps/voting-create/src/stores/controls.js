4950cfcb89582e36b979cccfae2419d4b0cfaffd
djembuka <djembuka@gmail.com>
2025-12-29 11:45:56 +0300

    feat(voting-create): add lang BX.message

--- bitrix-extensions/local/js/local/vue-apps/voting-create/src/stores/controls.js
+++ bitrix-extensions/local/js/local/vue-apps/voting-create/src/stores/controls.js
@@ -3,263 +3,263 @@ import { defineStore } from 'ui.vue3.pinia';
 export const controlsStore = defineStore('controls', {
   state: () => ({
     blocks: [
-        {
-          id: 1,
-          heading: 'Общие данные',
-          controls: [
-            {
-              property: 'text',
-              id: 'votingCreate11',
-              name: 'NAME',
-              label: 'Название голосования *',
-              value: '',
-              required: true,
-            },
-            {
-              property: 'textarea',
-              id: 'votingCreate12',
-              name: 'ANNOUNCEMENT',
-              label: 'Текст анонса',
-              value: '',
-              required: false,
-            },
-            {
-              property: 'textarea',
-              id: 'votingCreate13',
-              name: 'DESCRIPTION',
-              label: 'Текст подробного описания',
-              value: '',
-              required: false,
-            },
-          ]
-        },
-        {
-          id: 2,
-          heading: 'Сроки',
-          controls: [
-            {
-              property: "datetime",
-              type: "single",
-              id: 'votingCreate21',
-              name: "DATE_FROM",
-              label: "Дата начала активности *",
-              value: "",
-              required: true,
-            },
-            {
-              property: "datetime",
-              type: "single",
-              id: 'votingCreate22',
-              name: "DATE_TO",
-              label: "Дата окончания активности *",
-              value: "",
-              required: true,
-            },
-            {
-              property: "datetime",
-              type: "single",
-              id: 'votingCreate23',
-              name: "DATE_FINISH",
-              label: "Дата окончания голосования *",
-              value: "",
-              required: true,
-            },
-          ]
-        },
-        {
-          id: 3,
-          heading: 'Технические',
-          controls: [
-            {
-              property: "num",
-              id: 'votingCreate31',
-              name: "NUM",
-              label: "Количество попыток *",
-              value: "1",
-              required: true,
-            },
-            {
-              property: "num",
-              id: 'votingCreate32',
-              name: "NUM",
-              label: "Количество голосующих",
-              value: "",
-              required: false,
-            },
-            {
-              property: "text",
-              id: 'votingCreate33',
-              name: "BUTTON",
-              label: "Сообщение у кнопки",
-              value: "",
-              required: false,
-            },
-            {
-              property: 'textarea',
-              id: 'votingCreate34',
-              name: 'MESSAGE',
-              label: 'Сообщение после голосования',
-              value: '',
-              required: false,
-            },
-            {
-              property: "num",
-              id: 'votingCreate35',
-              name: "SORT",
-              label: "Порядок размещения *",
-              value: "",
-              required: true,
-              hint_external: "Используйте цифры от 1 до N, порядок определяет от 1, сверху вниз. 1 всегда будет первым. Для удобства рекомендуем использовать 10, 20, 30, это позволит оперативно изменить порядок."
-            },
-          ]
-        },
-        {
-          id: 4,
-          heading: 'Доступы',
-          controls: [
-            {
-              property: "text",
-              id: 'votingCreate41',
-              name: "MEMBERS_ID",
-              label: "ID групп голосующих",
-              value: "",
-              required: false,
-            },
-            {
-              property: "text",
-              id: 'votingCreate42',
-              name: "MEMBERS_EIO_ID",
-              label: "ID групп голосующих ЕИО",
-              value: "",
-              required: false,
-            },
-            {
-              property: "text",
-              id: 'votingCreate43',
-              name: "MEMBERS_UMC_ID",
-              label: "ID групп голосующих УМЦ",
-              value: "",
-              required: false,
-            },
-            {
-              property: "text",
-              id: 'votingCreate44',
-              name: "COMMISSIONS_ID",
-              label: "ID групп Избирательных комиссий",
-              value: "",
-              required: false,
-            },
-          ]
-        },
-        {
-          id: 5,
-          heading: 'Оформление документов',
-          controls: [
-            {
-              property: "text",
-              id: 'votingCreate51',
-              name: "DOC_TITLE",
-              label: "Заголовок документа",
-              value: "",
-              required: false,
-            },
-            {
-              property: "textarea",
-              id: 'votingCreate52',
-              name: "DOC_SUBTITLE",
-              label: "Подзаголовок документа",
-              value: "",
-              required: false,
-            },
-          ]
-        }
-      ]
+      {
+        id: 1,
+        heading: window.BX.message('TWPX_VOTING_CREATE_BLOCK1_HEADING'),
+        controls: [
+          {
+            property: 'text',
+            id: 'votingCreate11',
+            name: 'NAME',
+            label: window.BX.message('TWPX_VOTING_CREATE_NAME_LABEL'),
+            value: '',
+            required: true,
+          },
+          {
+            property: 'textarea',
+            id: 'votingCreate12',
+            name: 'ANNOUNCEMENT',
+            label: window.BX.message('TWPX_VOTING_CREATE_ANNOUNCEMENT_LABEL'),
+            value: '',
+            required: false,
+          },
+          {
+            property: 'textarea',
+            id: 'votingCreate13',
+            name: 'DESCRIPTION',
+            label: window.BX.message('TWPX_VOTING_CREATE_DESCRIPTION_LABEL'),
+            value: '',
+            required: false,
+          },
+        ],
+      },
+      {
+        id: 2,
+        heading: window.BX.message('TWPX_VOTING_CREATE_BLOCK2_HEADING'),
+        controls: [
+          {
+            property: 'datetime',
+            type: 'single',
+            id: 'votingCreate21',
+            name: 'DATE_FROM',
+            label: window.BX.message('TWPX_VOTING_CREATE_DATE_FROM_LABEL'),
+            value: '',
+            required: true,
+          },
+          {
+            property: 'datetime',
+            type: 'single',
+            id: 'votingCreate22',
+            name: 'DATE_TO',
+            label: window.BX.message('TWPX_VOTING_CREATE_DATE_TO_LABEL'),
+            value: '',
+            required: true,
+          },
+          {
+            property: 'datetime',
+            type: 'single',
+            id: 'votingCreate23',
+            name: 'DATE_FINISH',
+            label: window.BX.message('TWPX_VOTING_CREATE_DATE_FINISH_LABEL'),
+            value: '',
+            required: true,
+          },
+        ],
+      },
+      {
+        id: 3,
+        heading: window.BX.message('TWPX_VOTING_CREATE_BLOCK3_HEADING'),
+        controls: [
+          {
+            property: 'num',
+            id: 'votingCreate31',
+            name: 'NUM',
+            label: window.BX.message('TWPX_VOTING_CREATE_NUM_LABEL'),
+            value: '1',
+            required: true,
+          },
+          {
+            property: 'num',
+            id: 'votingCreate32',
+            name: 'VOTERS_NUM',
+            label: window.BX.message('TWPX_VOTING_CREATE_VOTERS_NUM_LABEL'),
+            value: '',
+            required: false,
+          },
+          {
+            property: 'text',
+            id: 'votingCreate33',
+            name: 'BUTTON',
+            label: window.BX.message('TWPX_VOTING_CREATE_BUTTON_LABEL'),
+            value: '',
+            required: false,
+          },
+          {
+            property: 'textarea',
+            id: 'votingCreate34',
+            name: 'MESSAGE',
+            label: window.BX.message('TWPX_VOTING_CREATE_MESSAGE_LABEL'),
+            value: '',
+            required: false,
+          },
+          {
+            property: 'num',
+            id: 'votingCreate35',
+            name: 'SORT',
+            label: window.BX.message('TWPX_VOTING_CREATE_SORT_LABEL'),
+            value: '',
+            required: true,
+            hint_external: window.BX.message('TWPX_VOTING_CREATE_SORT_HINT'),
+          },
+        ],
+      },
+      {
+        id: 4,
+        heading: window.BX.message('TWPX_VOTING_CREATE_BLOCK4_HEADING'),
+        controls: [
+          {
+            property: 'text',
+            id: 'votingCreate41',
+            name: 'MEMBERS_ID',
+            label: window.BX.message('TWPX_VOTING_CREATE_MEMBERS_ID_LABEL'),
+            value: '',
+            required: false,
+          },
+          {
+            property: 'text',
+            id: 'votingCreate42',
+            name: 'MEMBERS_EIO_ID',
+            label: window.BX.message('TWPX_VOTING_CREATE_MEMBERS_EIO_ID_LABEL'),
+            value: '',
+            required: false,
+          },
+          {
+            property: 'text',
+            id: 'votingCreate43',
+            name: 'MEMBERS_UMC_ID',
+            label: window.BX.message('TWPX_VOTING_CREATE_MEMBERS_UMC_ID_LABEL'),
+            value: '',
+            required: false,
+          },
+          {
+            property: 'text',
+            id: 'votingCreate44',
+            name: 'COMMISSIONS_ID',
+            label: window.BX.message('TWPX_VOTING_CREATE_COMMISSIONS_ID_LABEL'),
+            value: '',
+            required: false,
+          },
+        ],
+      },
+      {
+        id: 5,
+        heading: window.BX.message('TWPX_VOTING_CREATE_BLOCK5_HEADING'),
+        controls: [
+          {
+            property: 'text',
+            id: 'votingCreate51',
+            name: 'DOC_TITLE',
+            label: window.BX.message('TWPX_VOTING_CREATE_DOC_TITLE_LABEL'),
+            value: '',
+            required: false,
+          },
+          {
+            property: 'textarea',
+            id: 'votingCreate52',
+            name: 'DOC_SUBTITLE',
+            label: window.BX.message('TWPX_VOTING_CREATE_DOC_SUBTITLE_LABEL'),
+            value: '',
+            required: false,
+          },
+        ],
+      },
+    ],
   }),
   actions: {
     changeBlocks(blocks) {
       this.blocks = blocks;
     },
     changeTextControlValue({ control, value }) {
-        control.value = value;
+      control.value = value;
     },
     changeSelectRadioValue({ control, value }) {
-        control.value = value;
+      control.value = value;
     },
     changeSelectDropdownValue({ control, value }) {
-        control.value = value;
+      control.value = value;
     },
     changeDateValue({ control, value }) {
-        control.value = value;
+      control.value = value;
     },
     changeFileValue({ control, value }) {
-        control.value = value;
-        if (control.type === 'upload') {
-            this.uploadFile(control, value);
-        }
+      control.value = value;
+      if (control.type === 'upload') {
+        this.uploadFile(control, value);
+      }
     },
     changeControlValue({ control, value, checked }) {
-        switch (control.property) {
+      switch (control.property) {
         case 'text':
         case 'textarea':
         case 'hint':
         case 'tel':
         case 'email':
         case 'num':
-            this.changeTextControlValue({ control, value });
-            break;
+          this.changeTextControlValue({ control, value });
+          break;
         case 'select':
-            this[
+          this[
             `changeSelect${control.type
-                .substring(0, 1)
-                .toUpperCase()}${control.type.substring(1).toLowerCase()}Value`
-            ]({ control, value });
-            break;
+              .substring(0, 1)
+              .toUpperCase()}${control.type.substring(1).toLowerCase()}Value`
+          ]({ control, value });
+          break;
         case 'file':
-            this.changeFileValue({ control, value });
-            break;
+          this.changeFileValue({ control, value });
+          break;
         case 'date':
         case 'datetime':
-            this.changeDateValue({ control, value });
-            break;
-        }
+          this.changeDateValue({ control, value });
+          break;
+      }
     },
     async runHints(control, action) {
-        const url = new URL(action, window.location.origin);
-        url.searchParams.append('s', control.value);
+      const url = new URL(action, window.location.origin);
+      url.searchParams.append('s', control.value);
 
-        try {
+      try {
         // Создаем AbortController для возможности отмены запроса
         const controller = new AbortController();
         const timeoutId = setTimeout(() => controller.abort(), 20000); // 20 секунд таймаут
 
         const response = await fetch(url, {
-            signal: controller.signal,
-            headers: {
-                'Content-Type': 'application/json',
-            },
+          signal: controller.signal,
+          headers: {
+            'Content-Type': 'application/json',
+          },
         });
 
         clearTimeout(timeoutId);
 
         if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
+          throw new Error(`HTTP error! status: ${response.status}`);
         }
 
         const result = await response.json();
 
         if (result.status === 'success' && result.data) {
-            this.setHints(control, result.data);
+          this.setHints(control, result.data);
         } else if (result.errors) {
-            this.error = result.errors[0]?.message
+          this.error = result.errors[0]?.message;
         } else {
-            this.error = 'Invalid response format';
+          this.error = 'Invalid response format';
         }
-        } catch (error) {
-        this.error = error?.message
-        }
+      } catch (error) {
+        this.error = error?.message;
+      }
     },
     setHints(control, value) {
-        control.hints = value;
+      control.hints = value;
     },
-  }
+  },
 });
--- bitrix-extensions/local/js/local/vue-apps/voting-create/src/stores/data.js
+++ bitrix-extensions/local/js/local/vue-apps/voting-create/src/stores/data.js
@@ -4,19 +4,19 @@ export const dataStore = defineStore('data', {
   state: () => ({
     id: `id${Math.floor(Math.random() * 1000)}`,
     customData: {},
-		lang: {
-			heading: 'Новое голосование',
-      editHeading: 'Изменить голосование',
-			cancelButton: 'Отменить',
-			sendButton: 'Создать',
-			editSendButton: 'Изменить',
-		},
+    lang: {
+      heading: window.BX.message('TWPX_VOTING_CREATE_HEADING'),
+      editHeading: window.BX.message('TWPX_VOTING_CREATE_EDIT_HEADING'),
+      cancelButton: window.BX.message('TWPX_VOTING_CREATE_CANCEL_BUTTON'),
+      sendButton: window.BX.message('TWPX_VOTING_CREATE_SEND_BUTTON'),
+      editSendButton: window.BX.message('TWPX_VOTING_CREATE_EDIT_SEND_BUTTON'),
+    },
     actions: {
-      'editVoting': ['twinpx:voting.form', 'editVoting'],
-      'voting': ['twinpx:voting.form', 'voting']
+      editVoting: ['twinpx:voting.form', 'editVoting'],
+      voting: ['twinpx:voting.form', 'voting'],
     },
     votingDetailURL: '',
-    voting: {}
+    voting: {},
   }),
   actions: {
     changeProp(prop, value) {
@@ -24,18 +24,23 @@ export const dataStore = defineStore('data', {
     },
     runBitrixMethod(method, data) {
       if (!this.actions[method] || !Array.isArray(this.actions[method])) {
-        return Promise.reject({errors: [{message: `No such method ${method}`}]});
+        return Promise.reject({
+          errors: [{ message: `No such method ${method}` }],
+        });
       }
 
-      return BX.ajax.runComponentAction(this.actions[method][0], this.actions[method][1], {
-        mode: 'class',
-        data: {
-          ...this.customData,
-          ...data
-        },
-        signedParameters: this.signedParameters,
-      })
-    }
-  }
+      return BX.ajax.runComponentAction(
+        this.actions[method][0],
+        this.actions[method][1],
+        {
+          mode: 'class',
+          data: {
+            ...this.customData,
+            ...data,
+          },
+          signedParameters: this.signedParameters,
+        }
+      );
+    },
+  },
 });
-
